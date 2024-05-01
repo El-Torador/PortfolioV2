bc3fcb8506e5fb79c1b67d4db3235ebfb7b28d7a
KAGMENI Jordan <kagmeni@et.intechinfo.fr>
2024-05-01 12:12:08 +0200

    add GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,17 +4,35 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* <!-- Google Tag Manager --> */}
+        <Script id="GTM">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P2Z2F6ZZ');
+          `}
+        </Script>
+        {/* <!-- End Google Tag Manager --> */}
+      </Head>
       <body>
         <Script id="theme-switcher" strategy="beforeInteractive">
           {`
-            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)){
               document.documentElement.classList.add('dark')
             } else {
               document.documentElement.classList.remove('dark')
             }
           `}
         </Script>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2Z2F6ZZ"
+            height="0" width="0" style="display:none;visibility:hidden">
+          </iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Main />
         <NextScript />
       </body>

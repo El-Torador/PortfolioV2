a55a8d6e04008e417e089ed059f07a6fb0a38b5e
KAGMENI Jordan <kagmeni@et.intechinfo.fr>
2024-05-13 00:33:13 +0200

    add swebsite of school education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,10 +4,10 @@ import LiIcon from "./LiIcon";
 
 /**
  * DETAILS COMPONENT
- * @param {{ type: string, time: string, place: string, info: string, address: string }} props 
+ * @param {{ type: string, time: string, place: string, info: string, address: string, schoolLink: string }} props 
  * @returns {JSX.Element}
  */
-const Details = ({ type, time, place, info, address }) => {
+const Details = ({ type, time, place, info, address, schoolLink }) => {
   const ref = useRef(null);
   return <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]">
     <LiIcon reference={ref} />
@@ -16,12 +16,16 @@ const Details = ({ type, time, place, info, address }) => {
       whileInView={{ y: 0 }}
       transition={{ duration: 0.5, type: "spring" }}
     >
-      <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
-        {type}
-      </h3>
-      <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
-        {time} · {place}
-      </span>
+      <a href={schoolLink}
+        target="_blank"
+      >
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {type}
+        </h3>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
+          {time} · {place}
+        </span>
+      </a>
       <p className="font-medium w-full md:text-sm">
         {info}
       </p>
@@ -51,6 +55,7 @@ function Education() {
           type="Software Engineering Expert Degree"
           time="2022 - 2024"
           place="ESIEA"
+          schoolLink="https://www.esiea.fr/"
           address="Ivry sur seine, France."
           info="Creation of an IOS meeting application with automatic transcription, Implementation of a data structure to structure the data contained in a large log file and read it quickly to optimize the API response time and implement AI model Classifier."
         />
@@ -58,6 +63,7 @@ function Education() {
           type="Bachelor's degree in Computer Science"
           time="2021 - 2022"
           place="ESIEA"
+          schoolLink="https://www.esiea.fr/"
           address="Ivry sur seine, France."
           info="Graduate Bachelor in Computer Science after an internship of 6 months and a validated defense."
         />
@@ -65,6 +71,7 @@ function Education() {
           type="Bachelor's degree in Computer Science"
           time="2017 - 2020"
           place="Institut Africain Informatique"
+          schoolLink="https://web.campusiai.com"
           address="Yaounde, Cameroon."
           info="Graduated as a Computer Works Engineer after 3 years of intense studies and academic projects carried out. Obtaining an Excellent mention during the defense."
         />
